26e4f497f0325651779981ab9be23b95de1e9b52
med.wf <med.wf95@gmail.com>
2024-01-09 00:41:15 +0100

    task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,16 @@
-# Add a custom HTTP header with Puppet
+# instal and configure nginx
 exec {'update':
   command => '/usr/bin/apt-get update',
 }
-package { 'nginx':
+-> package { 'nginx':
   ensure => installed,
 }
-file_line { 'header_served_by':
+-> file_line { 'header_served_by':
   path  => '/etc/nginx/sites-available/default',
   match => '^server {',
   line  => "server {\n\tadd_header X-Served-By \"${hostname}\";",
   multiple => false,
 }
-exec {'run':
+-> exec {'run':
   command => '/usr/sbin/service nginx restart',
 }
