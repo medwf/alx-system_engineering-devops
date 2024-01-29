19be81164a7cd8141614d47670b1b939a135a416
med.wf <med.wf95@gmail.com>
2024-01-29 19:27:20 +0100

    w

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,19 +15,16 @@ def GET(id):
     EMPLOYEE_NAME = USERS.get('name')
     TOTAL_NUMBER_OF_TASKS = list(filter(
         lambda x: x.get('userId') == id,
-        TODOS
-    ))
+        TODOS))
     NUMBER_OF_DONE_TASKS = list(filter(
         lambda x: x.get('completed'),
-        TOTAL_NUMBER_OF_TASKS
-        ))
+        TOTAL_NUMBER_OF_TASKS))
     print("Employee {} is done with tasks({}/{}):".format(
         EMPLOYEE_NAME, len(NUMBER_OF_DONE_TASKS),
-        len(TOTAL_NUMBER_OF_TASKS)
-    ))
-    for TODO_DONE in NUMBER_OF_DONE_TASKS:
+        len(TOTAL_NUMBER_OF_TASKS)))
+    for TASK_TITLE in NUMBER_OF_DONE_TASKS:
         print("\t {}".format(
-            TODO_DONE.get('title')
+            TASK_TITLE.get('title')
         ))
 
 
