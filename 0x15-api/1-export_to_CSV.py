3e4a2e6ed1afaa5bf3a2f4f60f13deb20592c120
med.wf <med.wf95@gmail.com>
2024-01-29 20:52:23 +0100

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""import module
+"""
+import re
+import requests
+import sys
+
+
+def GET(id):
+    """GET : is function that get requests and print with right format"""
+    URL = f'https://jsonplaceholder.typicode.com'
+    USERS = requests.get(f"{URL}/users/{id}").json()
+    TODOs = requests.get(f"{URL}/todos").json()
+    EMPLOYEE_NAME = USERS.get('name')
+    # print(f"{USERS}\n{TODOS}\n{EMPLOYEE_NAME}")
+    TODOS = list(filter(
+        lambda x: x.get('userId') == id,
+        TODOs))
+    with open('{}.csv'.format(id), 'w') as file:
+        for TODO in TODOS:
+            file.write('"{}","{}","{}","{}"\n'.format(
+                id,
+                EMPLOYEE_NAME,
+                TODO.get('completed'),
+                TODO.get('title')))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and re.fullmatch(r'\d+', sys.argv[1]):
+        id = int(sys.argv[1])
+        GET(id)
