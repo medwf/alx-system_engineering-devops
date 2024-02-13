fcb304b6ef56ac0407f202dfb2323fc2eeca8bb9
med.wf <med.wf95@gmail.com>
2024-02-13 15:21:52 +0100

    all task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,10 +11,12 @@ header = {
 
 def number_of_subscribers(subreddit):
     """method doc"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, allow_redirects=False, headers=header)
-    if response.status_code == 200:
-        data = response.json()
-        return data["data"]["subscribers"]
-    else:
+    try:
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        response = requests.get(url, allow_redirects=False, headers=header)
+        if response.status_code == 200:
+            data = response.json()
+            return data["data"]["subscribers"]
+        return 0
+    except Exception:
         return 0
