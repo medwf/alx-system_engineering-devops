1455f5117bd86fcb1b3c6fc34ac1206f68bda297
med.wf <med.wf95@gmail.com>
2024-02-13 14:39:45 +0100

    task 0 up

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,15 @@ this doc for module
 """
 import requests
 
-header = {"User-Agent": "MyCustomUserAgent/1.0"}
+header = {
+    "User-Agent": "MyCustomUserAgent/1.0"
+}
 
 
 def number_of_subscribers(subreddit):
     """method doc"""
-    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(URL, allow_redirects=False, headers=header)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False, headers=header)
     if response.status_code == 200:
         data = response.json()
         return data["data"]["subscribers"]
