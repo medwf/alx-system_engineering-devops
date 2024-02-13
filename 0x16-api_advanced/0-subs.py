b0df0817d31c6ee983a9949f5e1ed9a7d608d5a3
med.wf <med.wf95@gmail.com>
2024-02-13 13:04:01 +0100

    task 0 up

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """return the number of subscribers"""
-    headers = {"User-Agent": "Chrome"}
+    headers = {"User-Agent": "MyCustomUserAgent/1.0"}
     URL = f"https://www.reddit.com/r/{subreddit}/about.json"
     data = requests.get(URL, allow_redirects=False, headers=headers)
     if data.status_code == 200:
