d0ae46964f2c4f9c58bcc43ad2df4f8eaeb6f28d
med.wf <med.wf95@gmail.com>
2024-02-13 13:10:31 +0100

    task 0 up

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""number_of_subscribers"""
+"""
+number_of_subscribers
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """return the number of subscribers"""
     headers = {"User-Agent": "MyCustomUserAgent/1.0"}
-    URL = f"https://www.reddit.com/r/{subreddit}/about.json"
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     data = requests.get(URL, allow_redirects=False, headers=headers)
     if data.status_code == 200:
         json_format = data.json()
         return json_format["data"]["subscribers"]
-    else:
-        return 0
+    return 0
